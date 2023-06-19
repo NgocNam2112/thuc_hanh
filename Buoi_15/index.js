23392103f33698fcb2df26a5ce27a0cf0792d60b
NgocNam2112 <namnguyen2k2112@gmail.com>
2023-06-19 20:57:27 +0700

    api practice session 16

--- Buoi_15/index.js
+++ Buoi_15/index.js
@@ -1,8 +1,42 @@
+const container = document.getElementById("container");
+
+const BASE_URL = "https://6487122fbeba6297278fd770.mockapi.io/testing";
+
 const fetchApi = async () => {
-  const { data } = await axios.get(
-    "https://6487122fbeba6297278fd770.mockapi.io/testing"
-  );
-  console.log("data", data);
+  try {
+    const { data } = await axios.get(BASE_URL);
+    container.innerHTML = data
+      .map((item) => {
+        return `
+        <div key="${item.id}" class="c__item" >
+              <img
+                src="${item.avatar}"
+                alt="avatar"
+              />
+              <h2>${item.name}</h2>
+              <p>${item.createdAt}</p>
+              <div class="c__btn">
+                <button>Detail</button>
+                <button onclick="handleDetele(${JSON.stringify(item.id)
+                  .split('"')
+                  .join("&quot;")})">Delete</button>
+              </div>
+            </div>
+        `;
+      })
+      .join(" ");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handleDetele = async (itemId) => {
+  try {
+    const { data } = await axios.delete(`${BASE_URL}/${itemId}`);
+    if (data !== null) {
+      fetchApi();
+    }
+  } catch (error) {}
 };
 
 fetchApi();
